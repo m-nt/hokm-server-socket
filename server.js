acb9ec5f8b9b185ea56924fb4e954402deb29e2d
mohamad nosratian <68176509+mohamadnosratian@users.noreply.github.com>
2022-01-30 00:11:43 +0330

    update

--- server.js
+++ server.js
@@ -82,11 +82,11 @@ io.on("connection", (socket) => {
   });
 });
 io.of("/").adapter.on("create-room", (room) => {
-  //console.log(`room ${room} was created`);
+  console.log(`room ${room} was created`);
 });
 
 io.of("/").adapter.on("join-room", (room, id) => {
-  //console.log(`socket ${id} has joined room ${room}`);
+  console.log(`socket ${id} has joined room ${room}`);
 });
 server.listen(PORT, () => {
   console.log(`listening on ${PORT} . . .`);
